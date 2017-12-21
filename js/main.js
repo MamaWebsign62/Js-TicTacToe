b38a50f37a3504372053ec5a1dd792a8093ee965
MamaWebsign62 <mregnault.netgyver@gmail.com>
2017-12-21 16:15:00 +0100

    Modif js 2ème partie

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ for (var i = 0; i < table.length; i++) {  // i=0; si i est plus petit que la lon
 
 
 function play(id){ //J'appelle la fonction play
+  console.log(id + " " + player);
   if(player == 1){  // Si c'est le joueur 1 qui a cliqué
     document.getElementById(id).classList.add("player1"); //Alors on ajoute la classe pour placer le signe
     table[id] = player;
@@ -19,7 +20,7 @@ function play(id){ //J'appelle la fonction play
     player=1;  // C'est au tour du joueur 1
   }
   document.getElementById(id).onclick = function() { return false; } // Le clic du joueur 2 ne fonctionnera pas
-  document.getElementById('player').innerHTML = 'Joueur '+player; //Affiche dans le HTML quel joueur doit jouer
+  document.getElementById('player').innerHTML = 'C\'est au tour du joueur '+player; //Affiche dans le HTML quel joueur doit jouer
 }
 
 function testVictoire() { // J'appelle la fonction pour tester la victoire
@@ -37,28 +38,26 @@ var victory = false;
     } else if(table[1] == player && table[4] == player && table[7] == player) // Si la case 1 = case 4 = case 7 (pour les les 2 joueurs)
     {
       victory = true;
-    } else if(table[2] == player && table[5] == player && table[8] == player){ // Si la case 2 = case 5 = case 8 (pour les les 2 joueurs)
+    } else if(table[2] == player && table[5] == player && table[8] == player) // Si la case 2 = case 5 = case 8 (pour les les 2 joueurs)
+    {
       victory = true;
-    } else if(table[3] == player && table[4] == player && table[5] == player){ // Si la case 3 = case 4 = case 5 (pour les les 2 joueurs)
+    } else if(table[3] == player && table[4] == player && table[5] == player) // Si la case 3 = case 4 = case 5 (pour les les 2 joueurs)
+    {
       victory = true;
-    } else if(table[6] == player && table[7] == player && table[8] == player){ // Si la case 6 = case 7 = case 8 (pour les les 2 joueurs)
+    } else if(table[6] == player && table[7] == player && table[8] == player) // Si la case 6 = case 7 = case 8 (pour les les 2 joueurs)
+    {
       victory = true;
-    } else if(table[2] == player && table[4] == player && table[6] == player){ // Si la case 2 = case 4 = case 6 (pour les les 2 joueurs)
+    } else if(table[2] == player && table[4] == player && table[6] == player) // Si la case 2 = case 4 = case 6 (pour les les 2 joueurs)
+    {
       victory = true;
     }
 
     if (victory) { // Si on appelle la variable "victory"
       document.getElementById('bingo').innerHTML = "Félicitations... Le player " + player + " a gagné !!!"; // Rappel de la div #bingo du html vers le js
-      document.getElementById('bingo').classList.add("visible"); // Appel de la class (css) pour rendre la div visible en cas de victoire
-      document.getElementById('bingo').classList.remove("hidden"); // Appel de la class (css) pour cacher la div lorsque l'on clique sur le message
+      document.getElementById('bingo').classList.remove("hidden"); // J'enlève la class (css) pour afficher la div en cas de victoire
       document.getElementById('bingo').onclick = function() // Lorsque l'on clique sur le message
       {
-        document.getElementById('bingo').classList.add("hidden");
-        document.getElementById('bingo').classList.remove("visible");
+        document.getElementById('bingo').classList.add("hidden"); // Le message de victoire disparaît lorsqu'on clique dessus
       }
     }
   }
-  //
-  // function afficheNumberCellule {
-  //
-  // }
